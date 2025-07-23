01332f6b3a5773c66992a7b711efa49bc7e30ce3
Sabari1239 <sabarivasan1239@gmail.com>
2025-07-23 20:34:30 +0530

    Updated app.js

--- server/app.js
+++ server/app.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const corsMiddleware = require('./config/cors');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(corsMiddleware);
 
 // Connect to MongoDB
 connectDB();
 
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
 // Basic route
 app.get('/', (req, res) => {
   res.send('API is running');
 });
 
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
 // TODO: Add routes for users, cars, bookings
 
 const PORT = process.env.PORT || 5000;
