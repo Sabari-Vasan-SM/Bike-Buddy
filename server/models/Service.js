9d13b9e46929a9b91f797ff239ad9898b2e840e2
Sabari1239 <sabarivasan1239@gmail.com>
2025-07-24 09:30:55 +0530

    Updated Services.js

--- server/models/Service.js
+++ server/models/Service.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose")
 
 const ServiceSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   duration: { type: Number, required: true },
-  description: { type: String }
-});
+  description: { type: String },
+})
 
-module.exports = mongoose.model('Service', ServiceSchema);
+module.exports = mongoose.model("Service", ServiceSchema)
