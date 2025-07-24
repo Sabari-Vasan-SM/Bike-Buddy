678f91bef9a68e785eb6eb46e467cd432ff7bdd8
Sabari1239 <sabarivasan1239@gmail.com>
2025-07-24 09:29:17 +0530

    Updated app.js

--- server/app.js
+++ server/app.js
@@ -1,26 +1,25 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const corsMiddleware = require('./config/cors');
+const express = require("express")
+const connectDB = require("./config/db")
+const corsMiddleware = require("./config/cors")
 
-const app = express();
+const app = express()
 
 // Middleware
-app.use(express.json());
-app.use(corsMiddleware);
+app.use(express.json())
+app.use(corsMiddleware)
 
 // Connect to MongoDB
-connectDB();
+connectDB()
 
 // Basic route
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
-
+app.get("/", (req, res) => {
+  res.send("API is running")
+})
 
 // Routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/services', require('./routes/services'));
-// TODO: Add routes for users, cars, bookings
+app.use("/api/auth", require("./routes/auth"))
+app.use("/api/services", require("./routes/services"))
+app.use("/api/bookings", require("./routes/bookings")) // Added bookings route
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
