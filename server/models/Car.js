de35bfbb3ff7c45f2f774e6d3d0ff98d2d7a88dd
Sabari1239 <sabarivasan1239@gmail.com>
2025-07-24 09:30:39 +0530

    Updated Car.js

--- server/models/Car.js
+++ server/models/Car.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose")
 
 const CarSchema = new mongoose.Schema({
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   make: { type: String, required: true },
   model: { type: String, required: true },
   year: { type: Number, required: true },
   price: { type: Number, required: true },
   available: { type: Boolean, default: true },
-  createdAt: { type: Date, default: Date.now }
-});
+  createdAt: { type: Date, default: Date.now },
+})
 
-module.exports = mongoose.model('Car', CarSchema);
+module.exports = mongoose.model("Car", CarSchema)
