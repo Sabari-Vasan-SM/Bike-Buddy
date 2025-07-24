3da3d4b0b0b2db16654fe3dea983a5ac99b3f238
Sabari1239 <sabarivasan1239@gmail.com>
2025-07-24 09:30:04 +0530

    Updated cors.js

--- server/config/cors.js
+++ server/config/cors.js
@@ -1,6 +1,6 @@
 // Enable CORS for http://localhost:3000
-const cors = require('cors');
+const cors = require("cors")
 module.exports = cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-});
+  origin: "http://localhost:3000",
+  credentials: true,
+})
