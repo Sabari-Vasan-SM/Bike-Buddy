cf7edf8a3efd7573e18140978446c898bb8d0fd8
Sabari1239 <sabarivasan1239@gmail.com>
2025-07-28 19:38:09 +0530

    Updated cors.js

--- server/config/cors.js
+++ server/config/cors.js
@@ -3,7 +3,7 @@ const cors = require("cors")
 const allowedOrigins = [
   "http://localhost:3000", // React default dev server
   "http://localhost:3001", // If you use this port
-  "https://cartrabbit-1-p9a2.onrender.com",
+  "https://cartrabbit-1-p9a2.onrender.com/api/auth/register",
   "127.0.0.1:3000"
   // add any other allowed origins here
 ];
